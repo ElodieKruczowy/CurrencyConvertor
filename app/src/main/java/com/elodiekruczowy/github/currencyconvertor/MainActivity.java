ff110f595c64f5f47805996fde669acd3ea3ed89
ElodieKruczowy <elodie.kruczowy@gmail.com>
2016-10-24 16:15:38 +0200

    TP2 - The value is converted upon pression of the button

--- app/src/main/java/com/elodiekruczowy/github/currencyconvertor/MainActivity.java
+++ app/src/main/java/com/elodiekruczowy/github/currencyconvertor/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     // Graphical elements from the Activity
     TextView convert_in, convert_out;
     Button convert_but;
+    // Conversion rate
+    float rate = 1.42f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +24,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.d(DEBUG_TAG, "onStart() function entered");
+        // Button to convert values
         convert_but = (Button) findViewById(R.id.button_convert);
+        convert_but.setOnClickListener(new convertButtonListener(this));
+
         convert_in = (TextView) findViewById(R.id.textfield_convIn);
         convert_out = (TextView) findViewById(R.id.textfield_convRes);
+    }
+
+    public void updateConversionValue() {
+        String txtValue;
+        float decValue;
+        txtValue = (convert_in.getText()).toString();
+        try {
+            decValue = Float.parseFloat(txtValue);
+            txtValue = (decValue * rate) + "€";
+        }
+        catch(Exception e) {
+            txtValue = "Wrong input! (NaN)";
+        }
+        convert_out.setText(txtValue);
     }
 }
